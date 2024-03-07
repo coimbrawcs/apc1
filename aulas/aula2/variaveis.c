484535333c3531b38c229a56f868cc7e2e448b02
coimbrawcs <160796218+coimbrawcs@users.noreply.github.com>
2024-03-07 00:47:32 +0000

    codigos da aula2 e pratica1

---
+++ aulas/aula2/variaveis.c
@@ -0,0 +1,17 @@
+iclude <stdio.h>
+
+int main() {
+  int numero;
+  float flutuante;
+  double juro_simples;
+
+  int a;
+ int x1,x2; // as raizes da equacao do 1° grau
+
+  numero = 10;
+
+  numero = 100;
+
+  return 0;
+  
+}
