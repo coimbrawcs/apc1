3dbde8c12af614e15dc2b6825ce2cd37be617736
coimbrawcs <160796218+coimbrawcs@users.noreply.github.com>
2024-03-14 00:47:11 +0000

    codigo da aula3 e pratica2

---
+++ aulas/aula3/notalegal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+printf("\x1b[32m----------\n");
+printf("NOTA LEGAL \x1b[0m\n");
+printf("ITEM    QTD VALOR \n");
+printf("%-13s %03i %8.2f\n","Banana nanica",1, 15.00);
+printf("%-13s %03i %8.2f\n","Maca fuji",10, 50.00);
+printf("%-13s %03i %8.2f\n","Uva globo",5, 26.00); 
+//printf("Banana nanica 1  15.00\n");
+//printf("Maca fuji     10  50.00\n");
+//printf("Uva globo     5  26.00\n");
+printf("--------------\n");
+printf("\x1b[34mTotal......: R$ %8.2f\x1b[0m\n", 91.00);
+  
+  
+
+
+return 0 ;
+}
+    
